2a4896d654ba65c019ada5543ba51bb2714011c3
MatheusJLV <matheusjlv@gmail.com>
2020-11-30 09:48:49 -0500

    ajuste de umbral rojo

--- ManejoImagen.py
+++ ManejoImagen.py
@@ -57,6 +57,9 @@ while(True):
         cv2.waitKey(0)
 
         img=roi
+        
+        
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
         cv2.imshow("Imagen", img)
         cv2.waitKey(0)
--- pruebaHSV.py
+++ pruebaHSV.py
@@ -7,7 +7,9 @@ while True:
     _, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # Red color
-    low_red = np.array([161, 155, 84])
+    #low_red = np.array([161, 155, 84])
+    #high_red = np.array([179, 255, 255])
+    low_red = np.array([161, 50, 50])
     high_red = np.array([179, 255, 255])
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
     red = cv2.bitwise_and(frame, frame, mask=red_mask)
